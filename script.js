7b7deebcf52f1508019217d09c50ea3929cc0e66
Chi Doan <chi.doan86@gmail.com>
2022-03-01 21:32:09 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+
+const generateBtn = document.querySelector("#generate");
+
+// when I click on the generate btn
+
+generateBtn.addEventListener('click', function() {
+
+console.log('hello')
+})
+
+// the app will ask me my password length
+const length = prompt("What is the password length?")
+
+//it will ask if I want to use
+//lowercase
+//uppercase
+//symbols
+//numbers
+//in my password
+//once I have selected all the options
+//then the app should generate the password
+//then show the generated password in the textarea
